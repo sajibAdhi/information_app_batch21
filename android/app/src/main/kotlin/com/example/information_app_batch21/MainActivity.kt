1befa305fc45d849a886ebe53a18a571af20b404
Sajib Adhikary <tosajibadhi@gmail.com>
2021-09-14 21:37:30 -0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/information_app_batch21/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.information_app_batch21
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: InfoPage(),
+    );
+  }
+}
+
+class InfoPage extends StatefulWidget {
+  const InfoPage({Key? key}) : super(key: key);
+
+  @override
+  _InfoPageState createState() => _InfoPageState();
+}
+
+class _InfoPageState extends State<InfoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                child: Card(
+                  child: Column(
+                    children: [
+                      CustomHeader(text: "Personal Information"),
+                      CustomField(text: "Personal Details", icon: Icons.person),
+                      CustomField(text: "Contact", icon: Icons.phone),
+                      CustomField(text: "Career Application Information", icon: Icons.file_copy),
+                      CustomField(text: "Preferred Area", icon: Icons.location_city),
+                      CustomField(text: "Other Relevant Information", icon: Icons.info_rounded),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                child: Card(
+                  child: Column(
+                    children: [
+                      CustomHeader(text: "Education / Training"),
+                      CustomField(text: "Academic Qualification", icon: Icons.build),
+                      CustomField(text: "Training Summary", icon: Icons.model_training),
+                      CustomField(text: "Professional Certificate Summary", icon: Icons.file_download),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                child: Card(
+                  child: Column(
+                    children: [
+                      CustomHeader(text: "Employment  History"),
+                      CustomField(text: "Personal Details", icon: Icons.person),
+                      CustomField(text: "Contact", icon: Icons.phone),
+                      CustomField(text: "Career Application Information", icon: Icons.file_copy),
+                      CustomField(text: "Preferred Area", icon: Icons.location_city),
+                      CustomField(text: "Other Relevant Information", icon: Icons.info_rounded),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                child: Card(
+                  child: Column(
+                    children: [
+                      CustomHeader(text: "Personal Information"),
+                      CustomField(text: "Personal Details", icon: Icons.person),
+                      CustomField(text: "Contact", icon: Icons.phone),
+                      CustomField(text: "Career Application Information", icon: Icons.file_copy),
+                      CustomField(text: "Preferred Area", icon: Icons.location_city),
+                      CustomField(text: "Other Relevant Information", icon: Icons.info_rounded),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomHeader extends StatelessWidget {
+  final String text;
+  const CustomHeader({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 180),
+      padding: EdgeInsets.all(10),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 18,
+        ),
+      ),
+    );
+  }
+}
+
+class CustomField extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  const CustomField({Key? key, required this.text, required this.icon}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(
+        text,
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),),
+    );
+  }
+}
+
+
